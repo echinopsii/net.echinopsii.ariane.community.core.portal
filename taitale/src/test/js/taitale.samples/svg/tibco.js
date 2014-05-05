b9ad8f47535a8bd30a9f8ffb5aa092bdb5cc62ce
Mathilde Ffrench <ffrench.mathilde@gmail.com>
2014-05-05 17:30:37 +0200

    [TAC-100] demo

--- taitale/src/test/js/taitale.samples/svg/tibco.js
+++ taitale/src/test/js/taitale.samples/svg/tibco.js
@@ -26,20 +26,20 @@ requirejs (
             var rightPath = rsr.path(
                 "M 97,524 C 97,524 110,511 95,487 C 95,487 81,466 69,455 L 2,455 " +
                     "L 2,609 L 28,609 C 35,601 74,555 79,548 C 79,548 94,531 97,524");
-            //rightPath.transform("t-2,-453s0.2t-200,-317t"+x*5+","+y*5)
+            rightPath.transform("t-2,-453s0.2t-200,-317t"+x*5+","+y*5)
             rset.push(rightPath);
 
             var middlePath = rsr.path(
                 "M 147,532 C 147,532 152,516 135,494 C 135,494 126,483 94,455 L 75,455 " +
                     "C 78,459 91,471 99,482 C 99,482 118,504 105,527 C 105,527 98,540 41,609 " +
                     "L 101,609 C 106,602 129,569 132,562 C 132,562 143,545 147,532");
-            //middlePath.transform("t-2,-453s0.2t-200,-317t-160,0t"+x*5+","+y*5)
+            middlePath.transform("t-2,-453s0.2t-200,-317t-160,0t"+x*5+","+y*5)
             rset.push(middlePath);
 
             var leftPath = rsr.path(
                 "M 137,487 C 137,487 161,510 154,536 C 154,536 153,549 114,609 L 161,609" +
                     "L 161,455 L 100,455 C 105,460 128,478 137,487");
-            //leftPath.transform("t-2,-453s0.2t-200,-317t-295,0t"+x*5+","+y*5)
+            leftPath.transform("t-2,-453s0.2t-200,-317t-295,0t"+x*5+","+y*5)
             rset.push(leftPath);
         }
 
@@ -68,5 +68,5 @@ requirejs (
             componentLogo.attr({fill: color,'fill-rule':'nonzero',stroke:'none','stroke-width':'1','stroke-opacity':'1'});
         }
 
-        componentType(200,400,'blue','Tibco','Tibco Rendez Vous','RVRD');
+        componentType(0,0,'blue','Tibco','Tibco Rendez Vous','RVRD');
     });
